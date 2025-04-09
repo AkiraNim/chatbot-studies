976c754d12bbf4726ccc511e48bc13a75b51cdb2
Akiranim <152340630+AkiraNim@users.noreply.github.com>
2025-04-09 20:52:10 -0300

    update v2

---
+++ ChatBot/Ver-2/chatbot_v2.js
@@ -0,0 +1,16 @@
+// leitor de qr code
+const qrcode = require('qrcode-terminal');
+const { Client, Buttons, List, MessageMedia } = require('whatsapp-web.js'); // Mudança Buttons
+const client = new Client();
+// serviço de leitura do qr code
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+// apos isso ele diz que foi tudo certo
+client.on('ready', () => {
+    console.log('Tudo certo! WhatsApp conectado.');
+});
+// E inicializa tudo 
+client.initialize();
+
+const delay = ms => new Promise(res => setTimeout(res, ms)); // Função que usamos para criar o delay entre uma ação e outra
